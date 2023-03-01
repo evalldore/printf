4ffb4d47d79f221dd8bff61ca478af8a694d73dd
evalldore <evalldore@gmail.com>
2023-03-01 15:43:54 -0500

    putaddr recursion and supports smaller hex numbers

--- src/ft_putaddr_fd.c
+++ src/ft_putaddr_fd.c
@@ -6,34 +6,31 @@
 /*   By: niceguy <niceguy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 00:41:35 by niceguy           #+#    #+#             */
-/*   Updated: 2023/03/01 05:40:41 by niceguy          ###   ########.fr       */
+/*   Updated: 2023/03/01 15:42:57 by niceguy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_writeaddr_fd(uint64_t num, int fd)
+{
+	char			ch;
+	int				i;
+
+	i = 1;
+	ch = (num & 0x0F) + '0';
+	if (ch > '9')
+		ch += 0x27;
+	if (num >= 16)
+		i += ft_writeaddr_fd((num >> 4), fd);
+	write(fd, &ch, 1);
+	return (i);
+}
+
 int	ft_putaddr_fd(void *ptr, int fd)
 {
-	uint64_t		num;
-	int				i;
-	uint8_t			ch;
-	char			add[15];
-
 	if (!ptr)
 		return (ft_putstr_fd("(nil)", 1));
-	num = (uint64_t)ptr;
-	i = 13;
-	(void)ft_memcpy(add, "0x000000000000", 15);
-	while (i >= 2)
-	{
-		ch = (num & 0x0F) + '0';
-		if (ch > '9')
-			ch += 0x27;
-		add[i] = ch;
-		num >>= 4;
-		i--;
-	}
-	add[15] = '\0';
-	write(fd, &add[0], 14);
-	return (14);
+	write(fd, "0x", 2);
+	return (2 + ft_writeaddr_fd((uint64_t)ptr, fd));
 }
